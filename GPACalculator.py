b6f1945811f00c42f4c2757d7042e7c6eacd9966
Seth Troll <trollseth011@gmail.com>
2017-08-01 19:46:23 -0500

    Added functions to file for data manipulation

--- GPACalculator.py
+++ GPACalculator.py
@@ -1,5 +1,6 @@
-from xlwt import Workbook
 """
+from xlwt import Workbook
+
 wb = Workbook()
 sheetname = 'Test Sheet'
 message = 'This is an example'
@@ -20,5 +21,51 @@ worksheet = workbook.sheet_by_name('Sheet1')
 grades = []
 for x in range(worksheet.nrows - 1):
     grades += worksheet.cell_value(x+1, 1)
+print(grades)
 
-print(grades[2] + grades[3])
+def findMinus(list):
+    for i in list:
+        if i == '-':
+            minus_index = list.index('-')
+            list.append(list[minus_index-1] + list[minus_index])
+            list.pop(minus_index)
+            list.pop(minus_index-1)
+    return list
+
+def findPlus(list):
+    for i in list:
+        if i == '+':
+            plus_index = list.index('+')
+            list.append(list[plus_index-1] + list[plus_index])
+            list.pop(plus_index)
+            list.pop(plus_index-1)
+    return list
+
+for i in range(2):
+    findMinus(grades)
+    findPlus(grades)
+
+def findValue(updatedlist):
+    file = open("grade_scale.txt", "r")
+    d = {}
+    grade_value = []
+    for line in file:
+        x = line.split(" ")
+        a = x[0]
+        b = x[1]
+        c=len(b)-1
+        b=b[0:c]
+        d[a] = b
+    for i in updatedlist:
+        print("this is i: " + i)
+        for x in d:
+            if i == x:
+                grade_value.append(float(d[x]))
+                print( grade_value)
+    total = sum(grade_value)
+    possible = len(grade_value)*4
+    print(total)
+    print(possible)
+
+
+findValue(grades)
